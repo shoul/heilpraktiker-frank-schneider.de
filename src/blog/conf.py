925836ee0952851cc8c13f9ec7a8b22e0d7ac755
Frank Schneider <fs@shoul.de>
2013-07-15 11:08:30 +0200

    Change blog path to /blog

--- src/blog/conf.py
+++ src/blog/conf.py
@@ -9,17 +9,18 @@ AUTHOR = 'Frank Schneider'
 EMAIL = 'info@heilpraktiker-frank-schneider.de'
 
 FILTERS = ['markdown+codehilite(css_class=highlight)', 'hyphenate', 'h1']
+
 VIEWS = {
     '/blog/': {'filters': 'summarize', 'view': 'index',
-          'pagination': '/page/:num/'},
+          'pagination': '/blog/page/:num/'},
 
     '/blog/:year/:slug/': {'views': ['entry', 'draft']},
 
     '/blog/tag/:name/': {'filters': 'summarize', 'view':'tag',
-                    'pagination': '/tag/:name/:num/'},
+                    'pagination': '/blog/tag/:name/:num/'},
 
     '/blog/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atom'},
-    '/blog/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rss'},
+    #'/blog/rss/': {'filters': ['h2', 'nohyphenate'], 'view': 'rss'},
 
     # # per tag Atom or RSS feed. Just uncomment to generate them.
     # '/tag/:name/atom/': {'filters': ['h2', 'nohyphenate'], 'view': 'atompertag'},
@@ -32,7 +33,7 @@ VIEWS = {
     # # Here are some more examples
 
     # # '/:slug/' is a slugified url of your static page's title
-    '/:slug/': {'view': 'page'},
+    '/:slug/': {'view': 'page', 'template': 'page.html'},
 
     # # '/atom/full/' will give you a _complete_ feed of all your entries
     '/blog/atom/full/': {'filters': 'h2', 'view': 'atom', 'num_entries': 1000},
@@ -49,10 +50,15 @@ VIEWS = {
     # '/:year/:slug/:lang/': {'view': 'translation'},
 }
 
-THEME = 'theme'
+THEME = 'simple_light'
+ENTRY_PERMALINK = '/:year/:slug/index.html'
+PAGE_PERMALINK = '/:slug/index.html'
+TAG_CLOUD_STEPS = 4
+TAG_CLOUD_MAX_ITEMS = 100
+TAG_CLOUD_START_INDEX = 0
+TAG_CLOUD_SHUFFLE = False
 ENGINE = 'acrylamid.templates.jinja2.Environment'
 DATE_FORMAT = '%d.%m.%Y, %H:%M'
 DEPLOYMENT = {
     'default': 'rsync -av $OUTPUT_DIR hpfs@ursa.uberspace.de:~/html/',
 }
-
