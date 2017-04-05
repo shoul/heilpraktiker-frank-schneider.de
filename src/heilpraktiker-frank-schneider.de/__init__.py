7789f58413597ea861a6bba588b698ca7e13f259
Frank Schneider <fs@shoul.de>
2017-04-05 21:55:35 +0200

    Add config via envvar variable from shell.

--- src/heilpraktiker-frank-schneider.de/__init__.py
+++ src/heilpraktiker-frank-schneider.de/__init__.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template
 
 app = Flask(__name__)
+app.config.from_envvar('HPFS_SETTINGS')
 
 @app.route('/')
 def index():
 	return render_template('base.html')
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	app.run()
