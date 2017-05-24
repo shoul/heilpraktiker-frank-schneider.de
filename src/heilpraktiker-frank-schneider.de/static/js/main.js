03e5d30e3df3ec743ba18a064d730f42eba208af
Frank Schneider <fs@shoul.de>
2017-05-24 23:36:02 +0200

    Fix mobile first.

--- src/heilpraktiker-frank-schneider.de/static/js/main.js
+++ src/heilpraktiker-frank-schneider.de/static/js/main.js
@@ -1,3 +1,12 @@
+function toggleNavbar() {
+  var n = document.getElementById("navbar-small");
+  if (n.className.indexOf("w3-show") == -1) {
+    n.className += " w3-show";
+  } else {
+    n.className = n.className.replace(" w3-show", "");
+  }
+}
+/*
 $(document).ready(function () {
 	view = new ol.View({
 	  	center: [1373295.0134457604, 6680221.679608975],
@@ -33,3 +42,4 @@ $(document).ready(function () {
 	map.addLayer(point);
 
 });
+*/
