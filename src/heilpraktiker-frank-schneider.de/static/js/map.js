8ecc7786d0d5f9ccb593c56d53072bef4c9441dd
Frank Schneider <fs@shoul.de>
2017-05-26 14:20:44 +0200

    Add custom pointer to map.

--- src/heilpraktiker-frank-schneider.de/static/js/map.js
+++ src/heilpraktiker-frank-schneider.de/static/js/map.js
@@ -1,11 +1,17 @@
 
-var point = new ol.layer.Vector({
+var pointer = new ol.layer.Vector({
 	source: new ol.source.Vector({
 		features: [
 			new ol.Feature({
 				geometry: new ol.geom.Point([1373285.013457604, 6680225.679608975])
 			})
 		]
+	}),
+	style: new ol.style.Style({
+		image: new ol.style.Icon({
+			anchor: [0.5, 1],
+			src: '/static/img/Marker.svg.png',
+		})
 	})
 });
 
@@ -15,7 +21,7 @@ var map = new ol.Map({
     new ol.layer.Tile({
       source: new ol.source.OSM()
     }),
-  	point
+  	pointer
   ],
   view: new ol.View({
     center: ol.proj.fromLonLat([12.33646, 51.33194]),
